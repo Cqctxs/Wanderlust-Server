a2d20f8f28dcffdc99a542ec91a65f5d8c01f56c
Sean <seanzhao2007@gmail.com>
2024-08-01 00:13:56 -0400

    Removed redundant dotenv config

--- controllers/api/generateController.js
+++ controllers/api/generateController.js
@@ -7,7 +7,7 @@ const {
   HarmBlockThreshold,
   FunctionDeclarationSchemaType,
 } = require("@google/generative-ai");
-require("dotenv").config();
+
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
