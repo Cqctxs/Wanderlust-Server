a580735d6b91c655787e52f661e0962ee8b14407
Sean <seanzhao2007@gmail.com>
2024-08-05 16:30:30 -0400

    change user schema

--- data/User.js
+++ data/User.js
@@ -2,23 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    roles: {
-        User: {
-            type: Number,
-            default: 1001
-        },
-        Editor: Number,
-        Admin: Number
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    refreshToken: String,
+    id: String,
     previousGenerations: [Object]
 });
 
