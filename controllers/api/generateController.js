c01807ae795fd6b87f5ccee4bf2f21e14dd6cd6f
Sean <seanzhao2007@gmail.com>
2024-08-05 17:06:37 -0400

    fixed route

--- controllers/api/generateController.js
+++ controllers/api/generateController.js
@@ -1,6 +1,6 @@
 const { Client } = require("@googlemaps/google-maps-services-js");
 const client = new Client({});
-const User = require("../data/User");
+const User = require("../../data/User");
 
 const {
   GoogleGenerativeAI,
