3da0cc06d4959e28c4a93365d1cd543fc8c290b4
eogito <glenlin7813@gmail.com>
2024-08-12 13:55:28 -0400

    deleted jojo :(

--- routes/api/jojothewarrior.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jojothewarriorController = require("../../controllers/api/jojothewarriorController")
-
-router.get('/', jojothewarriorController.getItinerary);
-
-module.exports = router;
